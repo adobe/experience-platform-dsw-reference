5cb79d5aba18df55d72da0f245e443f1698d1cc0
jonaphin <jonaphin@gmail.com>
2018-08-17 13:07:58 -0700

    remove login steps from build steps

--- recipes/R/Retail - GradientBoosting/build.sh
+++ recipes/R/Retail - GradientBoosting/build.sh
@@ -5,20 +5,6 @@ echo "please enter the version number for your recipe's docker image"
 
 read version
 
-echo "Enter Docker Host"
-
-read host
-
-echo "Enter Docker Username"
-
-read username
-
-echo "enter Docker Password"
-
-read password
-
-docker login --username $username --password $password $host
-
 docker build -t $host/ml-retail-r:$version .
 docker push $host/ml-retail-r:$version
 
