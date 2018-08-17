63290fc74ecff0ffdf65f2cf17a535e72bfcb169
jonaphin <jonaphin@gmail.com>
2018-08-17 13:03:41 -0700

    standardize build.sh

--- recipes/R/Retail - GradientBoosting/build.sh
+++ recipes/R/Retail - GradientBoosting/build.sh
@@ -1,21 +1,25 @@
 echo ""
 echo ""
 
-echo "please enter the version number of docker"
+echo "please enter the version number for your recipe's docker image"
 
 read version
 
-echo "Enter docker username"
+echo "Enter Docker Host"
+
+read host
+
+echo "Enter Docker Username"
 
 read username
 
-echo "enter docker password"
+echo "enter Docker Password"
 
 read password
 
-docker login --username $username --password $password [DOCKER IMAGE BASE URL]
+docker login --username $username --password $password $host
 
-docker build -t [DOCKER IMAGE BASE URL]/ml-retail-r:$version .
-docker push [DOCKER IMAGE BASE URL]/ml-retail-r:$version
+docker build -t $host/ml-retail-r:$version .
+docker push $host/ml-retail-r:$version
 
-echo [DOCKER IMAGE BASE URL]/ml-retail-r:$version
+echo $host/ml-retail-r:$version
