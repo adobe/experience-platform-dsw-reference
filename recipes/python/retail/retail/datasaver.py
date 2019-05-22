d316b7301a8fd415b29383d8690e4d2e6f436056
Sathya Arun <sarun@adobe.com>
2019-05-22 13:34:04 -0700

    Removing datasaver catalog and ingestion urls which comes from service

--- recipes/python/retail/retail/datasaver.py
+++ recipes/python/retail/retail/datasaver.py
@@ -21,11 +21,7 @@ def save(configProperties, prediction):
     print("Datasaver Start")
     print("Setting up Writer")
 
-    #catalog_url = "https://platform.adobe.io/data/foundation/catalog"
-    #ingestion_url = "https://platform.adobe.io/data/foundation/import"
-
-    # writer = DataSetWriter(catalog_url=catalog_url,
-    #                       ingestion_url=ingestion_url,
+   
     writer = DataSetWriter(client_id=configProperties['ML_FRAMEWORK_IMS_USER_CLIENT_ID'],
                            user_token=configProperties['ML_FRAMEWORK_IMS_TOKEN'],
                            service_token=configProperties['ML_FRAMEWORK_IMS_ML_TOKEN'])
