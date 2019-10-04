cfdf87c7557d5145251580aa6eca9af6048efa4c
Milos Paunovic <end37053@adobe.com>
2019-10-04 13:24:14 +0200

    PLATML-5950 adding utils with get_client_context method

---
+++ recipes/python/retail/retail/utils.py
@@ -0,0 +1,26 @@
+#####################################################################
+# ADOBE CONFIDENTIAL
+# ___________________
+#
+#  Copyright 2019 Adobe
+#  All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains
+# the property of Adobe and its suppliers, if any. The intellectual
+# and technical concepts contained herein are proprietary to Adobe
+# and its suppliers and are protected by all applicable intellectual
+# property laws, including trade secret and copyright laws.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Adobe.
+#####################################################################
+
+from platform_sdk.client_context import ClientContext
+
+def get_client_context(config_properties):
+    return ClientContext(api_key=config_properties['ML_FRAMEWORK_IMS_USER_CLIENT_ID'],
+                         org_id=config_properties['ML_FRAMEWORK_IMS_TENANT_ID'],
+                         user_token=config_properties['ML_FRAMEWORK_IMS_TOKEN'],
+                         service_token=config_properties['ML_FRAMEWORK_IMS_ML_TOKEN'],
+                         sandbox_id=config_properties['sandboxId'],
+                         sandbox_name=config_properties['sandboxName'])
